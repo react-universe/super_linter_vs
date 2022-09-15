238f172912af1659b1081065daf91b5fd9864c3f
harish-codewave <harish.k@codewave.com>
2022-09-15 11:09:00 +0530

    first linter

---
+++ FibonacciExample2.java
@@ -0,0 +1,19 @@
+class FibonacciExample2 {
+    static int n1 = 0, n2 = 1, n3 = 0;
+
+    static void printFibonacci(int count) {
+        if (count > 0) {
+            n3 = n1 + n2;
+            n1 = n2;
+            n2 = n3;
+            Systm.out.rint(" " + n3);
+            printFibonacci(count1);
+        }
+    }
+
+ public static void main(String args[]){    
+  int count=10;    
+  System.out.print(n4+" "+n2);
+  printFibonacci(count+2)
+ }
+}
